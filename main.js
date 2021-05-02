15a91e5c25fc2fcc652a7c477c3dc069a5c4b4c5
Marcin Bielen <mbielen1995@gmail.com>
2021-05-02 12:32:38 +0200

    Adding tasks to list

--- main.js
+++ main.js
@@ -2,13 +2,21 @@
 const form = document.querySelector("form");
 const countTasks = document.querySelector("h1 span");
 const list = document.querySelector("ul");
-const listElements = document.querySelectorAll("li");
 
 function addTask(event) {
     event.preventDefault();  //zapobiega odswiezaniu strony
     const title = this.elements["taskName"].value; //pobieramy nazwe zadania
+    if (title === '') {  //alert i wychodzenie z funkcji w przypadku nie podania nazwy zadania
+        alert("Podaj nazwe zadania!");
+        return;
+    }
     this.elements["taskName"].value = ""; //zerujemy input formularza
-    console.log(title)
+    
+    const task = document.createElement("li");
+    task.classList.add("task");
+    task.textContent = title;
+    list.appendChild(task);
+    countTasks.textContent = list.childElementCount;
 }
 
 //klikniecie na przycisk wysyla formularz
