fe391f74fe93f27ad0e62a159606305985275d53
Marcin Bielen <mbielen1995@gmail.com>
2021-05-03 09:04:39 +0200

    Add comments

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//pobieramy przyciski do usuwania zadan
 const doneButtons = document.querySelectorAll("li .done");
 
+//usuwamy zadanie
 function removeTask() {
     const id = this.parentNode.dataset.key;
     document.querySelector(`li[data-key="${id}"]`).remove();
