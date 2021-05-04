61b43f9c368d9c854dd667bcc6f0185bb16d8f3d
Marcin Bielen <mbielen1995@gmail.com>
2021-05-04 14:01:25 +0200

    Merge adding and deleting

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const form = document.querySelector("form");
 const countTasks = document.querySelector("h1 span");
 const list = document.querySelector("ul");
+let doneButtons = document.querySelectorAll("li .done");
+countTasks.textContent = list.childElementCount;
+let nextIndex = list.childElementCount++;
 
 function addTask(event) {
     event.preventDefault();  //zapobiega odswiezaniu strony
@@ -12,12 +15,28 @@ function addTask(event) {
     }
     this.elements["taskName"].value = ""; //zerujemy input formularza
     
+    nextIndex++;
     const task = document.createElement("li");
-    task.classList.add("task");
-    task.textContent = title;
+    task.setAttribute("data-key", `${nextIndex}`);
+    task.innerHTML = title + ' <button class="done">usuń</button>';
     list.appendChild(task);
+    countTasks.textContent = list.childElementCount;
+    doneButtons = document.querySelectorAll("li .done");
+    doneButtons.forEach(function(button) {
+        button.addEventListener("click", removeTask);
+    })
+}
+
+//usuwamy zadanie
+function removeTask() {
+    const id = this.parentNode.dataset.key;
+    document.querySelector(`li[data-key="${id}"]`).remove();
     countTasks.textContent = list.childElementCount;
 }
 
 //klikniecie na przycisk wysyla formularz
 form.addEventListener("submit", addTask);
+
+doneButtons.forEach(function(button) {
+    button.addEventListener("click", removeTask);
+})
