4619496377f5b4f85feca712db79e7c692a91f07
Marcin Bielen <mbielen1995@gmail.com>
2021-05-04 13:19:27 +0200

    Add comments

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const searcher = document.querySelector(".searcher");
 const list = document.querySelector("ul");
 const listElements = list.querySelectorAll("li");
 
+//wyszukiwarka
 searcher.addEventListener("keyup", function() {
+    //zamieniamy na tablice liste elementow li
     let elements = [...listElements];
+    //filtrujemy te, ktore zawieraja szukana fraze
     elements = elements.filter(element => 
         element.textContent.toLowerCase().includes(searcher.value.toLowerCase()));
+    //wyswietlamy szukane
     list.innerHTML = "";
     elements.forEach(li => list.appendChild(li));
 })
